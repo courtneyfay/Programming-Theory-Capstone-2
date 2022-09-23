da641c276ba3b75c024073b465c6c7190c15a803
courtneyfay <courtney.fay@wagmo.io>
2022-09-23 12:36:02 -0600

    Making rabbit jump

--- Assets/Scripts/JumpButtonController.cs
+++ Assets/Scripts/JumpButtonController.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class JumpButtonController : MonoBehaviour
 {
+    RabbitController rabbitController;
+    [SerializeField] private GameObject rabbit;
+
+    void Awake()
+    {
+        rabbitController = rabbit.GetComponent<RabbitController>();
+    }
+
     // ENCAPSULATION - private variable is exposed in the inspector with SerializeField label
     [SerializeField]
     private void ClickJump()
     {
         Debug.Log("hitting JUMP button controller");
         // fox.Jump();
-        // rabbit.Jump();
+        rabbitController.Jump();
     }
 }
---
+++ Assets/Scripts/RabbitController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitController : Animal
+{
+    private Rigidbody rb;
+    
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // POLYMORPHISM - Rabbit class overrides virtual methods inherited from Animal class
+    public override void Walk() 
+    {
+        // Player.Health -= 2;
+        // CommitPettyTheft();
+        Debug.Log("Rabbit is walking");
+    }
+    public override void Jump()
+    {
+        // make the animal jump
+        Debug.Log("Rabbit is jumping");
+        rb.AddForce(Vector3.up * 300f);
+    }
+}
